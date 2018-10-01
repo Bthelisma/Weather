4485ef4e4caefc040cb77a9dfe6f4b22afcc7310
Beatrice Thelisma <jerseysure@MacBook-Pro-3.local>
2018-09-30 22:05:26 -0700

    first commit

---
+++ weatherApp/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpService } from './http.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { BurbankComponent } from './burbank/burbank.component';
+import { ChicagoComponent } from './chicago/chicago.component';
+import { DallasComponent } from './dallas/dallas.component';
+import { DcComponent } from './dc/dc.component';
+import { SeattleComponent } from './seattle/seattle.component';
+import { SanJoseComponent } from './san-jose/san-jose.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BurbankComponent,
+    ChicagoComponent,
+    DallasComponent,
+    DcComponent,
+    SeattleComponent,
+    SanJoseComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [HttpService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
